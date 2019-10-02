823c92ec2b7d4d24c5f779d4341cd5b29d7681e1
pragya-sharma <pragya,sharma_cs18@gla.ac.in>
2019-10-03 00:06:11 +0530

    convert input into lowercase

---
+++ src/Solutions/Ques6.java
@@ -0,0 +1,13 @@
+package Solutions;
+import java.util.Scanner;
+public class Ques6 {
+    public static void main(String[] artgs){
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter String Value : ");
+        String stringInput = scanner.nextLine();
+
+        stringInput = stringInput.toLowerCase();
+        System.out.println("Lowered Case : " + stringInput);
+
+    }
+}
