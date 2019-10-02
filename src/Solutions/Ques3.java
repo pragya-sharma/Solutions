8c0ef2ed18368fc674760c7a78b5919a07029780
pragya-sharma <pragya,sharma_cs18@gla.ac.in>
2019-10-02 23:41:19 +0530

    initial commit

---
+++ src/Solutions/Ques3.java
@@ -0,0 +1,17 @@
+package Solutions;
+import java.util.Scanner;
+public class Ques3 {
+        public static void main(String args[]){
+            Scanner scanner = new Scanner(System.in);
+            String inputString = scanner.nextLine();
+
+            inputString = inputString.toLowerCase();
+            inputString = inputString.replace('a','*');
+            inputString = inputString.replace('e','*');
+            inputString = inputString.replace('i','*');
+            inputString = inputString.replace('o','*');
+            inputString = inputString.replace('u','*');
+
+            System.out.println(inputString);
+        }
+}
