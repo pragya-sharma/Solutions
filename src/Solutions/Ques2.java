f92d7e63a59d0619e0e791ec7e4afc2d3f50cb41
pragya-sharma <pragya,sharma_cs18@gla.ac.in>
2019-10-03 13:19:22 +0530

    Some Modifications

--- src/Solutions/Ques2.java
+++ src/Solutions/Ques2.java
@@ -1,17 +1,20 @@
-/*
+package Solutions;
+/**
     WAP to print substring of a given inputString by using
     String substring(int index). Accept inputString and index from user.
  */
+
 import java.util.Scanner;
 public class Ques2 {
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
+
         System.out.printf("Please Enter any String Value : ");
         String stringInput = scanner.nextLine();
         System.out.printf("Enter the index for substring : ");
         int indexOfSubstring = scanner.nextInt();
         String substring = stringInput.substring(indexOfSubstring);
-        System.out.println("subString Value is "+substring);
+        System.out.println("subString Value is " + substring);
 
     }
 }
--- src/Solutions/Ques6.java
+++ src/Solutions/Ques6.java
@@ -1,7 +1,8 @@
 package Solutions;
+
 import java.util.Scanner;
 public class Ques6 {
-    public static void main(String[] artgs){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.printf("Enter String Value : ");
         String stringInput = scanner.nextLine();
@@ -11,3 +12,5 @@ public class Ques6 {
 
     }
 }
+
+
